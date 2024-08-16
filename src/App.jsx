d420dc1062dc7d94fd88950b17c04a77633c17ab
Ciarango10 <carlosarango3101@gmail.com>
2024-08-15 20:09:47 -0500

    Primer entregable FrontEndIII

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
+import { useState } from 'react'
 import './App.css'
 import Card from './Components/Card'
+import './Styles/Form.css'
 
 function App() {
+  const [payload, setPayload] = useState({
+    name: "",
+    password: ""
+  });
+  const [error, setError] = useState(false);
+  const [show, setShow] = useState(false);
+
+  const handleName = (e) => setPayload({...payload, name: e.target.value});
+  const handlePassword = (e) => setPayload({...payload, password: e.target.value});
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //Validar que no tenga espacios en blanco al inicio
+    const RegExp = /^\s/;
+
+    if(RegExp.test(payload.name) || payload.name.trim().length < 3 || payload.password.trim().length < 6) {
+      setError(true);
+      setShow(false);
+    } else {
+      setError(false);
+      setShow(true);
+    }
+  }
+
   return (
     <div className='app'>
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
+      {
+        show
+        ? <Card payload={payload} />
+        : <div>
+            <h1>Carga de estudiantes</h1>
+            <form onSubmit={handleSubmit} >
+              <div>
+                <label htmlFor="txtName">Nombre: </label>
+                <input type="text" id='txtName' onChange={handleName} value={payload.name} />
+              </div>
+              <div>
+                <label htmlFor="txtPassword">Contraseña: </label>
+                <input type="password" id='txtPassword' onChange={handlePassword} value={payload.password} />
+              </div>
+              <button type='submit'>Ingresar</button>
+            </form>
+          </div>
+      }  
+      { error 
+        ? <div><h3 style={{color:"red"}}>Por favor chequea que la información sea correcta</h3></div> 
+        : null
+      }
     </div>
   )
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import CardStyles from "../Styles/Card.module.css"
 
-const Card = () => {
+const Card = ({payload}) => {
   return (
-    <div>Hola soy un tarjeta</div>
+    <div className={CardStyles.userCard}>
+      <div className={CardStyles.userCardBody}>
+          <h2 style={{color:"#1d8b0d"}}>Login Exitoso!</h2>
+          <h3 className={CardStyles.userName}>Nombre</h3>
+          <p className={CardStyles.userInfo}>{payload.name}</p>
+          <h3 className={CardStyles.userPassword}>Contraseña</h3>
+          <p className={CardStyles.userInfo}>{payload.password}</p>
+      </div>
+    </div>
   )
 }
 
